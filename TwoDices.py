811b60dd7116b55b4632ab057b42782dbfffb097
Hussain Mohamed <40736864+HUMO8DY@users.noreply.github.com>
2020-12-12 23:43:27 +0200

    Add files via upload

---
+++ TwoDices.py
@@ -0,0 +1,231 @@
+import random
+print("This is dice stimulator")
+x = "y"
+
+while x == "y":
+    number = random.randint(1,21)
+
+    if number == 1: # 1 & 1
+        print("----------"  "----------")
+        print("|        |"  "|        |")
+        print("|    O   |"  "|    O   |")
+        print("|        |"  "|        |")
+        print("----------"  "----------")
+    if number == 2: # 1 & 2
+        print("----------"  "----------")
+        print("|        |"  "|        |")
+        print("|    O   |"  "| O    O |")
+        print("|        |"  "|        |")
+        print("----------"  "----------")
+    if number == 3: # 1 & 3
+        print("----------"  "----------")
+        print("|        |"  "|    O   |")
+        print("|    O   |"  "|    O   |")
+        print("|        |"  "|    O   |")
+        print("----------"  "----------")
+    if number == 4: # 1 & 4
+        print("----------"  "----------")
+        print("|        |"  "| O    O |")
+        print("|    O   |"  "|        |")
+        print("|        |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 5: # 1 & 5
+        print("----------"  "----------")
+        print("|        |"  "| O    O |")
+        print("|    O   |"  "|    O   |")
+        print("|        |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 6: # 1 & 6
+        print("----------"  "----------")
+        print("|        |"  "| O    O |")
+        print("|    O   |"  "| O    O |")
+        print("|        |"  "| O    O |")
+        print("----------"  "----------")
+
+    if number == 7: # 2 & 1
+        print("----------"  "----------")
+        print("|        |"  "|        |")
+        print("| O    O |"  "|    O   |")
+        print("|        |"  "|        |")
+        print("----------"  "----------")                        
+    if number == 8: # 2 & 2
+        print("----------"  "----------")
+        print("|        |"  "|        |")
+        print("| O    O |"  "| O    O |")
+        print("|        |"  "|        |")
+        print("----------"  "----------")
+    if number == 9:     # 2 & 3
+        print("----------"  "----------")
+        print("|        |"  "|    O   |")
+        print("| O    O |"  "|    O   |")
+        print("|        |"  "|    O   |")
+        print("----------"  "----------")
+    if number == 10:    # 2 & 4
+        print("----------"  "----------")
+        print("|        |"  "| O    O |")
+        print("| O    O |"  "|        |")
+        print("|        |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 11:    # 2 & 5
+        print("----------"  "----------")
+        print("|        |"  "| O    O |")
+        print("| O    O |"  "|    O   |")
+        print("|        |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 12:    # 2 & 6
+        print("----------"  "----------")
+        print("|        |"  "| O    O |")
+        print("| O    O |"  "| O    O |")
+        print("|        |"  "| O    O |")
+        print("----------"  "----------")
+
+    if number == 13:    # 3 & 1
+        print("----------"  "----------")
+        print("|    O   |"  "|        |")
+        print("|    O   |"  "|    O   |")
+        print("|    O   |"  "|        |")
+        print("----------"  "----------")
+    if number == 14:    # 3 & 2
+        print("----------"  "----------")
+        print("|    O   |"  "|        |")
+        print("|    O   |"  "| O    O |")
+        print("|    O   |"  "|        |")
+        print("----------"  "----------")    
+    if number == 15:    # 3 & 3
+        print("----------"  "----------")
+        print("|    O   |"  "|    O   |")
+        print("|    O   |"  "|    O   |")
+        print("|    O   |"  "|    O   |")
+        print("----------"  "----------")
+    if number == 16:    # 3 & 4
+        print("----------"  "----------")
+        print("|    O   |"  "| O    O |")
+        print("|    O   |"  "|        |")
+        print("|    O   |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 17:    # 3 & 5
+        print("----------"  "----------")
+        print("|    O   |"  "| O    O |")
+        print("|    O   |"  "|    O   |")
+        print("|    O   |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 18:    # 3 & 6
+        print("----------"  "----------")
+        print("|    O   |"  "| O    O |")
+        print("|    O   |"  "| O    O |")
+        print("|    O   |"  "| O    O |")
+        print("----------"  "----------")
+    
+    if number == 19:    # 4 & 1
+        print("----------"  "----------")
+        print("| O    O |"  "|        |")
+        print("|        |"  "|    O   |")
+        print("| O    O |"  "|        |")
+        print("----------"  "----------")
+    if number == 20:    # 4 & 2
+        print("----------"  "----------")
+        print("| O    O |"  "|        |")
+        print("|        |"  "| O    O |")
+        print("| O    O |"  "|        |")
+        print("----------"  "----------")
+    if number == 21:    # 4 & 3
+        print("----------"  "----------")
+        print("| O    O |"  "|    O   |")
+        print("|        |"  "|    O   |")
+        print("| O    O |"  "|    O   |")
+        print("----------"  "----------")
+    if number == 22:    # 4 & 4
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("|        |"  "|        |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 23:    # 4 & 5
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("|        |"  "|    O   |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 24:    # 4 & 6
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("|        |"  "| O    O |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+
+    if number == 25:    # 5 & 1
+        print("----------"  "----------")
+        print("| O    O |"  "|        |")
+        print("|    O   |"  "|    O   |")
+        print("| O    O |"  "|        |")
+        print("----------"  "----------")
+    if number == 26:    # 5 & 2
+        print("----------"  "----------")
+        print("| O    O |"  "|        |")
+        print("|    O   |"  "| O    O |")
+        print("| O    O |"  "|        |")
+        print("----------"  "----------")
+    if number == 27:    # 5 & 3
+        print("----------"  "----------")
+        print("| O    O |"  "|    O   |")
+        print("|    O   |"  "|    O   |")
+        print("| O    O |"  "|    O   |")
+        print("----------"  "----------")
+    if number == 28:    # 5 & 4
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("|    O   |"  "|        |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 29:    # 5 & 5
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("|    O   |"  "|    O   |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 30:    # 5 & 6
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("|    O   |"  "| O    O |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+
+    if number == 31:    # 6 & 1
+        print("----------"  "----------")
+        print("| O    O |"  "|        |")
+        print("| O    O |"  "|    O   |")
+        print("| O    O |"  "|        |")
+        print("----------"  "----------")
+    if number == 32:    # 6 & 2
+        print("----------"  "----------")
+        print("| O    O |"  "|        |")
+        print("| O    O |"  "| O    O |")
+        print("| O    O |"  "|        |")
+        print("----------"  "----------")
+    if number == 33:    # 6 & 3
+        print("----------"  "----------")
+        print("| O    O |"  "|    O   |")
+        print("| O    O |"  "|    O   |")
+        print("| O    O |"  "|    O   |")
+        print("----------"  "----------")
+    if number == 34:    # 6 & 4
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("| O    O |"  "|        |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 35:    # 6 & 5
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("| O    O |"  "|    O   |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+    if number == 36:    # 6 & 6
+        print("----------"  "----------")
+        print("| O    O |"  "| O    O |")
+        print("| O    O |"  "| O    O |")
+        print("| O    O |"  "| O    O |")
+        print("----------"  "----------")
+
+
+    x = input("Press y to roll again")
